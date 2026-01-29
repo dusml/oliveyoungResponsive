a0598551887a174c92c0ac46348432387c401c99
dusml <qkrthdus998@gmail.com>
2026-01-29 17:06:37 +0900

    추가 업로드

---
+++ script/main.js
@@ -0,0 +1,12 @@
+//모바일 슬라이드 메뉴
+const hamBtn = document.querySelector('.ham_menu')
+const headerNav = document.querySelector('.header_menu')
+const closeBtn = document.querySelector('.colse_btn')
+
+hamBtn.onclick = function(){
+  headerNav.style.right = '0'
+}
+
+closeBtn.addEventListener('click',function(){
+  headerNav.style.right = '-100%'
+})
